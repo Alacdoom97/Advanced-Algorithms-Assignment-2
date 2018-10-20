ac390cf68e9fe1a09a20e2f693926dee794e0109
Pranav <pranav.dilip.patel@gmail.com>
2018-10-20 17:37:35 +0200

    ex3 done

--- src/MeasureTest.java
+++ src/MeasureTest.java
@@ -2,18 +2,35 @@
 public class MeasureTest {
 
 	public static void main(String[] args) {
-		int[] array1 = new int[] {9, 3, 1, 0};
-		int[] array2 = new int[] {9, 3, 1, 0};
+		int[] array1 = new int[] {2, 5, 3, 9};
+		int[] array2 = new int[] {15, 12, 1, 3};
+		int[] array3 = new int[] {44, 44, 45, 32, 27, 34, 40, 47, 11, 37};
 		
-		MyMeasure mm = new MyMeasure();
+		MyMeasure mm = new MyMeasure(array1, array2);
 		
 		
-		if(mm.isSameCollection(array1, array2)) {
+		/*if(mm.isSameCollection(array1, array2)) {
 			System.out.println("The collection is the same!");
 		}
 		else {
 			System.out.println("The collections are not the same!");
+		}*/
+		int summ = mm.minDifferences(array1, array2);
+		if(summ == 0) {
+			System.out.println("");
+		} else {
+			System.out.println("sum of the differences squared: " + summ);
 		}
+		int[] temp = mm.getPercentileRange(array3, 25, 55);
+		System.out.print("[ ");
+		for(int i = 0; i < temp.length; i++) {
+			if (temp[i] != 0) {
+				System.out.print(temp[i] + " ");
+			}
+		}
+		System.out.println("]");
+		
+		
 	}
 
 }
--- src/MyMeasure.java
+++ src/MyMeasure.java
@@ -1,46 +1,137 @@
+import java.util.Arrays;
 
 public class MyMeasure implements A2Measure {
-	
+
 	private int min1, max1, min2, max2, median1, median2;
+	private int[] tempArray1, tempArray2;
+	
+	public MyMeasure(int[] array1, int[] array2) {
+		
+	}
 
 	@Override
 	public boolean isSameCollection(int[] array1, int[] array2) {
-		if(array1.length != array2.length) {
-		return false;
-		} else if (array1.equals(array2)) {
-			return true;
+		checkAndSort(array1, array2);
+		if (array1.length != array2.length) {
+			return false;
 		}
-		return false;
+		for (int i = 0; i < array1.length; i++) {
+			if(tempArray1[i] != tempArray2[i]) {
+				return false;
+			} else {
+				continue;
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public int minDifferences(int[] array1, int[] array2) {
-		// TODO Auto-generated method stub
-		return 0;
+		checkAndSort(array1, array2);
+		if (array1.length != array2.length) {
+			System.out.println("The 2 arrays' lengths are not the same!");
+			return 0;
+		}
+		int sum = 0;
+		
+		for (int i = 0; i < array1.length; i++) {
+			int diff = 0;
+			diff = tempArray1[i] - tempArray2[i];
+			sum += (diff*diff);
+		}
+		return sum	;
 	}
 
 	@Override
 	public int[] getPercentileRange(int[] arr, int lower, int upper) {
-		// TODO Auto-generated method stub
-		return null;
+		checkSingleArray(arr);
+		int[] percentArray = sortArray(arr, max1);
+		int[] newArray = new int[arr.length];
+		int arrLength = arr.length;
+		float fLower = lower;
+		float fHigher = upper;
+		int lowPer = Math.round(fLower/100 * arrLength);
+		int highPer = Math.round(fHigher/100 * arrLength);
+		
+		for (int i = lowPer; i < highPer; i++) {
+			newArray[i] = percentArray[i];
+		}
+		
+		
+		return newArray;
 	}
-	
+
 	public void checkArray(int[] arr, int[] arr2) {
 		int tempMin = 0;
+		int tempMin2 = 0;
 		int tempMax = 0;
+		int tempMax2 = 0;
 		for (int i : arr) {
-			if(i < tempMin ) {
+			if (i < tempMin) {
 				tempMin = i;
 				min1 = tempMin;
-			} else if( i > tempMax) {
+			} else if (i > tempMax) {
+				tempMax = i;
+				max1 = tempMax;
+			}
+		}
+		for (int i : arr2) {
+			if (i < tempMin2) {
+				tempMin2 = i;
+				min2 = tempMin2;
+			} else if (i > tempMax2) {
+				tempMax2 = i;
+				max2 = tempMax2;
+			}
+		}
+	}
+	
+	private void checkSingleArray(int[] arr) {
+		int tempMin = 0;
+		int tempMin2 = 0;
+		int tempMax = 0;
+		int tempMax2 = 0;
+		for (int i : arr) {
+			if (i < tempMin) {
+				tempMin = i;
+				min1 = tempMin;
+			} else if (i > tempMax) {
 				tempMax = i;
 				max1 = tempMax;
 			}
 		}
 	}
-	
-	private void sortArray(int[] arr) {
+
+	private int[] sortArray(int[] arr, int maxValue) {
 		
+	    int numCounts[] = new int[maxValue + 1];
+
+	    
+	    for (int num : arr) {
+	        numCounts[num]++;
+	    }
+
+	    int[] sortedArray = new int[arr.length];
+	    int currentSortedIndex = 0;
+
+	    
+	    for (int num = 0; num < numCounts.length; num++) {
+	        int count = numCounts[num];
+
+	        for (int i = 0; i < count; i++) {
+
+	            sortedArray[currentSortedIndex] = num;
+	            currentSortedIndex++;
+	        }
+	    }
+
+	    return sortedArray;
+	}
+	
+	private void checkAndSort(int [] array1, int [] array2) {
+		checkArray(array1, array2);
+		tempArray1 = sortArray(array1, max1);
+		tempArray2 = sortArray(array2, max2);
 	}
 
 }
